2444214485e681bb03dd2208e435abd1676ddfaf
Rahul <rahulmathur87@gmail.com>
2024-08-11 02:31:49 +0530

    improvements

--- main.py
+++ main.py
@@ -1,26 +1,14 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class MyForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
-    password = StringField('Password', validators=[DataRequired()])
+    email = StringField('Email')
+    password = PasswordField('Password')
+    submit = SubmitField(label='Log In')
 
-
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -39,3 +27,17 @@ def login():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+'''
+Red underlines? Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
