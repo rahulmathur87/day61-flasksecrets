3895677ec073aa397033745024887bbe0e5d7d77
Rahul <rahulmathur87@gmail.com>
2024-08-12 04:56:54 +0530

    add bootstrap-flask

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
+from flask_bootstrap import Bootstrap5
 
 
 class MyForm(FlaskForm):
@@ -12,6 +13,7 @@ class MyForm(FlaskForm):
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
+bootstrap = Bootstrap5(app)
 
 
 @app.route("/")
